7c4a0f36c354dbccbb73a8e4d0f13d4c7067367a
SurendarRUS <102970694+SurendarRUS@users.noreply.github.com>
2023-01-10 01:03:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,13 +12,34 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 <body>
-
-<?php require_once('php/header.php');?>
+<div class="row about container-fluid my-3 col-lg-10 col-xl-10 mx-auto">
+    <article class="col-xs-11 col-sm-11 col-md-6 col-lg-6 col-xl-6 mx-auto">
+        <h3 class="mx-auto">About Us</h3>
+        <p class="mx-auto">5g Constructions is one of the best builders for house construction in Chennai with a Vision of spanning across India, offering building solutions from start to finish. We service all of your construction needs, right from designing to execution, foundation to framing, interior works, and more!</p>
+        <p class="mx-auto">We understand that delivering projects is not just about square footage or scale; it’s about being trusted with the responsibility to bring an idea to life. Working back from the needs of our customers and building lasting client relationships is not something we take lightly!</p>
+        <div class="exp_nums mt-3 col-10 col-sm-10 col-md-11 col-lg-11 col-xl-11 d-flex align-items-center">
+    <div class="expr mx-3 d-flex flex-column justify-content-center ">
+        <h2 class="counter mx-auto" id="expr" data-target='4'></h2>
+        <p class="h6">Years of Experience</p>
+    </div>
+    <div class="prjs mx-3 d-flex flex-column justify-content-center ">
+        <h2 class="counter mx-auto" id="prjs" data-target='12'></h2>
+        <p class="h6">Projects</p>
+    </div>
+    </div>
+    </article>
+    <aside class="col-xs-11 col-sm-11 col-md-6 col-lg-6 col-xl-6 mx-auto d-flex justify-content-center">
+        <img class="mx-auto" src="https://images.pexels.com/photos/38293/workers-construction-site-hardhats-38293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+       
+    </aside>
+    
+</div>
+<!-- <?php require_once('php/header.php');?>
 <?php require_once('php/queryForm.php')?>
 <?php require_once('php/carousel.php');?>
 <?php require_once('php/about.php');?>
 <?php require_once('php/services.php');?>
-<?php require_once('php/footer.php');?>
+<?php require_once('php/footer.php');?> -->
 
 
 <script src="index.js"></script>
